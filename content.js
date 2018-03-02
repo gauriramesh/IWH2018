02b191e62dfed46cdee6dc88730e3c04b255f8cb
gauri <gauri.ramesh15@gmail.com>
2018-03-02 14:14:10 -0600

    Boilerplate for message passing

--- content.js
+++ content.js
@@ -12,3 +12,12 @@ chrome.runtime.onMessage.addListener(
 				});
 		}
 });
+
+var port = chrome.runtime.connect({name: "knockknock"});
+port.postMessage({joke: "Knock knock"});
+port.onMessage.addListener(function(msg) {
+  if (msg.question == "Who's there?")
+    port.postMessage({answer: "Madame"});
+  else if (msg.question == "Madame who?")
+    port.postMessage({answer: "Madame... Bovary"});
+});
